0ca4301a3286582004372b6aea5a53b2753e1f51
z3nth10n <z3nth10n@gmail.com>
2025-12-11 10:00:06 +0100

    new sounds for guitars

--- sounds/convert.sh
+++ sounds/convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p mp3
 for f in *.flac; do
-    ffmpeg -i "$f" -t 3 -acodec libmp3lame -b:a 320k "mp3/${f%.flac}.mp3"
+    ffmpeg -i "$f" -acodec libmp3lame -b:a 192k "mp3/${f%.flac}.mp3" -y
 done
